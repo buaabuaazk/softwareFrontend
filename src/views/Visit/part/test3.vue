2053a213402392e91dad5d0a42abe7abfb98acc7
weirdo <21371480@buaa.edu.cn>
2023-05-21 09:24:53 +0800

    reCommonQuestion

--- src/views/Visit/part/test3.vue
+++ src/views/Visit/part/test3.vue
@@ -1,4 +1,5 @@
 <template>
+    <!--  -->
     <div class="onboarding onboarding--loading">
         <div class="onboarding__item onboarding__item--active">
           <h1>Design System Principles</h1>
