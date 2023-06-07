f06418391baf50aba79b9c518a910038c5e6e577
weirdo <21371480@buaa.edu.cn>
2023-06-07 12:56:33 +0800

    resource

--- src/views/User/resource/personalFile.vue
+++ src/views/User/resource/personalFile.vue
@@ -18,20 +18,20 @@
         </el-table-column>
         <el-table-column label="操作" width="280px">
           <template v-slot="scope">
-            <!-- 下载按钮 -->
-          <!-- <el-button type="primary" content="下载" placement="top" :enterable="false" @click="DownloadResource(scope.row)" ><el-button type="warning" ></el-button><el-icon><Download /></el-icon></el-button> -->
-          <el-tooltip effect="dark" content="下载" placement="top" :enterable="false" @click="DownloadResource(scope.row)">
-            <el-button type="primary" @click="DownloadResource(scope.row)"><el-icon><Download /></el-icon></el-button> 
+            <!-- 查看按钮 -->
+            <el-tooltip effect="dark" content="查看" placement="top" :enterable="false" @click="getResource(scope.row)">
+              <el-button type="success" @click="getResource(scope.row)"><el-icon><Document /></el-icon></el-button> 
           </el-tooltip>
-          <!-- 删除按钮 -->
-          <!-- <el-button type="danger" content="删除" placement="top" :enterable="false" @click="deleteResource(scope.row)"><el-icon><delete /></el-icon></el-button> -->
-          <el-tooltip effect="dark" content="删除" placement="top" :enterable="false" @click="deleteResource(scope.row)">
-            <el-button type="danger"  @click="deleteResource(scope.row)"><el-icon><delete /></el-icon></el-button> 
+          <!-- 下载按钮 -->
+        <!-- <el-button type="primary" content="下载" placement="top" :enterable="false" @click="DownloadResource(scope.row)" ><el-button type="warning" ></el-button><el-icon><Download /></el-icon></el-button> -->
+        <el-tooltip effect="dark" content="下载" placement="top" :enterable="false" @click="DownloadResource(scope.row)">
+          <el-button type="primary" @click="DownloadResource(scope.row)"><el-icon><Download /></el-icon></el-button> 
           </el-tooltip>
-          <!-- 举报按钮 -->
-          <el-tooltip effect="dark" content="举报" placement="top" :enterable="false" @click="ReportResource(scope.row)">
-            <el-button type="warning" ><el-icon><WarnTriangleFilled /></el-icon></el-button> 
-          </el-tooltip>
+        <!-- 删除按钮 -->
+        <!-- <el-button type="danger" content="删除" placement="top" :enterable="false" @click="deleteResource(scope.row)"><el-icon><delete /></el-icon></el-button> -->
+        <el-tooltip effect="dark" content="删除" placement="top" :enterable="false" @click="deleteResource(scope.row)">
+          <el-button type="danger"  @click="deleteResource(scope.row)"><el-icon><delete /></el-icon></el-button> 
+       </el-tooltip>
           </template>
         </el-table-column>
       </el-table>
@@ -152,6 +152,100 @@ import { mapState, mapMutations } from 'vuex'
                 console.log(res.resource_info_list.length);
                 this.userlist = ref([...res.resource_info_list]);
                 console.log(this.userlist1);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+        // 查看资源
+        async getResource(row) {
+            try {
+                const { data: res } = await axios.post("http://81.70.17.242:8000/source/get", {
+                    "name": row.name
+                }
+                // ,{
+                        // headers: {
+                        //     Authorization: this.getToken_glo()
+                        // }
+                // }
+                );
+                // if (res.code !== 200)
+                //     return this.$message.error("获取用户列表失败");
+                console.log(res);
+                console.log(res.url);
+                window.open(res.url)
+                //var decodedString = decodeURIComponent(res);
+                //console.log(decodedString);
+                var element = document.getElementById("yourElementId");
+                //res_utf8 = res.decode('utf-8');
+                element.innerText = res;
+                //this.queryInfo.total = res.resource_info_list.length;
+                //console.log(res.resource_info_list);
+                //console.log(res.resource_info_list.length);
+                //console.log(this.userlist);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+        //删除资源
+        async deleteResource(row) {
+            try {
+                // console.log(row.name)
+                const { data: res } = await axios.post("http://81.70.17.242:8000/source/delete",
+                {"name": row.name},
+                {
+                    headers: {
+                        Authorization: this.getToken_glo()
+                    }
+                }
+                    );
+                    console.log(res)
+                this.getPersonalResource()
+                if (res.code == 200)
+                    return this.$message.success("删除成功");
+                else{
+                    return this.$message.error("删除失败");
+                }
+                //this.userlist = res.data.users;
+                //this.total = res.data.total;
+                //console.log(res);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+
+        //下载资源
+        async DownloadResource(row) {
+            try {
+                console.log(row.name)
+                //console.log(row.get('name'))
+                const { data: res } = await axios.post("http://81.70.17.242:8000/source/download",                    
+                        {"name": row.name}
+                    ,{
+                    headers: {
+                        Authorization: this.getToken_glo()
+                    }
+                });
+                // if (res.code == 200)
+                //     return this.$message.error("获取用户列表失败");
+                console.log(res);
+                console.log(res.url);
+                fetch(res.url)
+                .then(response => response.blob())
+                .then(blob => {
+                    const downloadLink = document.createElement('a');
+                    downloadLink.href = URL.createObjectURL(blob);
+                    downloadLink.download = row.name; // 可选：设置要保存的文件名
+                    downloadLink.click();
+                    URL.revokeObjectURL(downloadLink.href);
+                    this.$message.success('下载成功')
+                })
+                .catch(error => {
+                    console.error('文件下载失败', error);
+                });
+
             }
             catch (error) {
                 console.log(error);
